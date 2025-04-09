fdfcf716ed0c2dfc626c8b6f3662a88651ab95da
MS Studios <simomagy02@proton.me>
2025-04-09 22:32:43 +0200

    added speed insights for vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,63 +1,65 @@
-import "@/styles/globals.css";
-import { Metadata, Viewport } from "next";
-import { Link } from "@heroui/link";
-import clsx from "clsx";
+import '@/styles/globals.css';
+import { Metadata, Viewport } from 'next';
+import { Link } from '@heroui/link';
+import clsx from 'clsx';
 
-import { Providers } from "./providers";
+import { Providers } from './providers';
 
-import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
-import { Navbar } from "@/components/navbar";
+import { siteConfig } from '@/config/site';
+import { fontSans } from '@/config/fonts';
+import { Navbar } from '@/components/navbar';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  icons: {
-    icon: "/favicon.ico",
-  },
+	title: {
+		default: siteConfig.name,
+		template: `%s - ${siteConfig.name}`,
+	},
+	description: siteConfig.description,
+	icons: {
+		icon: '/favicon.ico',
+	},
 };
 
 export const viewport: Viewport = {
-  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "black" }],
+	themeColor: [{ media: '(prefers-color-scheme: dark)', color: 'black' }],
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html suppressHydrationWarning lang="en">
-      <head />
-      <body
-        className={clsx(
-          "min-h-screen bg-neutral-950 font-sans antialiased",
-          fontSans.variable,
-        )}
-      >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <div className="relative flex flex-col h-screen">
-            <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-              {children}
-            </main>
-            <footer className="w-full flex items-center justify-center py-6">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href={siteConfig.links.github}
-                title="BRS Github"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">Nebula Studios</p>
-              </Link>
-            </footer>
-          </div>
-        </Providers>
-      </body>
-    </html>
-  );
+	return (
+		<html suppressHydrationWarning lang="en">
+			<head />
+			<body
+				className={clsx(
+					'min-h-screen bg-neutral-950 font-sans antialiased',
+					fontSans.variable
+				)}
+			>
+				<Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
+					<div className="relative flex flex-col h-screen">
+						<Navbar />
+						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+							{children}
+							<SpeedInsights />
+						</main>
+						<footer className="w-full flex items-center justify-center py-6">
+							<Link
+								isExternal
+								className="flex items-center gap-1 text-current"
+								href={siteConfig.links.github}
+								title="BRS Github"
+							>
+								<span className="text-default-600">Powered by</span>
+								<p className="text-primary">Nebula Studios</p>
+							</Link>
+						</footer>
+					</div>
+				</Providers>
+			</body>
+		</html>
+	);
 }
